991fa0ddf70003f1a9a5b99f22444ea9c32bb756
Aleksey Kravetz <alex@alekseys-mbp.mynet>
2019-07-31 13:01:02 +0300

    added CDN comments, added test code

--- react-cdn/index.jsx
+++ react-cdn/index.jsx
@@ -25,3 +25,30 @@ class AppClassComponent extends React.Component {
 
 
 ReactDOM.render(<AppClassComponent />, document.querySelector('#root'));
+
+
+// class TitleComponent extends React.Component {
+// 	render() {
+// 		return (
+// 			<div style={{ fontSize: 50, backgroundColor: 'blue', }}>
+// 				<h1>Some Title</h1>
+// 			</div>
+// 		)
+// 	}
+// }
+
+// class ButtonComponent extends React.Component {
+// 	render() {
+// 		return (
+// 			<div onClick={this.props.onButtonClicked}>Click Me!</div>
+// 		);
+// 	}
+// }
+
+// class SomeParentComponent extends React.Component {
+// 	render() {
+// 		return (
+// 			<ButtonComponent onButtonClicked={() => console.log('Title clicked')} />
+// 		)
+// 	}
+// }
